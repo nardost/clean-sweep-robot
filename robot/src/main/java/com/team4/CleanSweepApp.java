a71865129b87d0178c456e3d64868626900e286f
nardos <32473919+nardost@users.noreply.github.com>
2019-10-19 18:49:07 -0500

    Time delay added in Robot.work() to simulate Robot staying in a tile while working.

--- robot/src/main/java/com/team4/CleanSweepApp.java
+++ robot/src/main/java/com/team4/CleanSweepApp.java
@@ -8,7 +8,5 @@ public class CleanSweepApp {
         Robot cleanSweepRobot = RobotCleanSweep.getInstance();
         cleanSweepRobot.turnOn();
         cleanSweepRobot.turnOff();
-        
-        
     }
 }
