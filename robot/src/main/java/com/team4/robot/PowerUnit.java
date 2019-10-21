55cb0ba569d82d4720bf3cd9bb250ffaec8d7afe
Nardos Tessema <32473919+nardost@users.noreply.github.com>
2019-10-21 10:13:55 -0500

    PowerUnit.java updated.

--- robot/src/main/java/com/team4/robot/PowerUnit.java
+++ robot/src/main/java/com/team4/robot/PowerUnit.java
@@ -4,36 +4,77 @@ import com.team4.commons.ConfigManager;
 
 public class PowerUnit implements PowerManager {
 
-	private int batteryLevel;
-	
-	private PowerUnit() {
-		int maxBatteryLevel = Integer.parseInt(ConfigManager.getConfiguration("maxBatteryLevel"));
-		setBatteryLevel(maxBatteryLevel);
-	}
-	
-	@Override
-	public void recharge() {
-		try {
-			//simulate delay while charging.
-			Thread.sleep(2000L);
-		} catch (InterruptedException ie) {
-			ie.printStackTrace();
-		}
-		int maxBatteryLevel = Integer.parseInt(ConfigManager.getConfiguration("maxBatteryLevel"));
-		setBatteryLevel(maxBatteryLevel);
-	}
+    private int batteryLevel;
 
-	@Override
-	public int getBatteryLevel() {
-		return batteryLevel;
-	}
+    private PowerUnit() {
+        int maxBatteryLevel = Integer.parseInt(ConfigManager.getConfiguration("maxBatteryLevel"));
+        setBatteryLevel(maxBatteryLevel);
+    }
 
-	@Override
-	public void updateBatteryLevel(int units) {
-		setBatteryLevel(getBatteryLevel() - units);
-	}
+    @Override
+    public void recharge() {
+        try {
+            //simulate delay while charging.
+            long timeToCharge = Long.parseLong(ConfigManager.getConfiguration("timeToCharge"));
+            Thread.sleep(timeToCharge * 1000L);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        int maxBatteryLevel = Integer.parseInt(ConfigManager.getConfiguration("maxBatteryLevel"));
+        setBatteryLevel(maxBatteryLevel);
+    }
 
-	private void setBatteryLevel(int batteryLevel) {
-		this.batteryLevel = batteryLevel;
-	}
+    @Override
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    @Override
+    public void updateBatteryLevel(int units) {
+        setBatteryLevel(getBatteryLevel() - units);
+    }
+
+    private void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+}
+package com.team4.robot;
+
+import com.team4.commons.ConfigManager;
+
+public class PowerUnit implements PowerManager {
+
+    private int batteryLevel;
+
+    private PowerUnit() {
+        int maxBatteryLevel = Integer.parseInt(ConfigManager.getConfiguration("maxBatteryLevel"));
+        setBatteryLevel(maxBatteryLevel);
+    }
+
+    @Override
+    public void recharge() {
+        try {
+            //simulate delay while charging.
+            long timeToCharge = Long.parseLong(ConfigManager.getConfiguration("timeToCharge"));
+            Thread.sleep(timeToCharge * 1000L);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        int maxBatteryLevel = Integer.parseInt(ConfigManager.getConfiguration("maxBatteryLevel"));
+        setBatteryLevel(maxBatteryLevel);
+    }
+
+    @Override
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    @Override
+    public void updateBatteryLevel(int units) {
+        setBatteryLevel(getBatteryLevel() - units);
+    }
+
+    private void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
 }
