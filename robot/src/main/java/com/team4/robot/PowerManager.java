d71de110aaccbb6662d00b81cd31865a184a00fb
nardos <32473919+nardost@users.noreply.github.com>
2019-10-21 09:56:35 -0500

    Power management classes copied from RaymondBranch.

--- robot/src/main/java/com/team4/robot/PowerManager.java
+++ robot/src/main/java/com/team4/robot/PowerManager.java
@@ -1,4 +1,7 @@
 package com.team4.robot;
 
 interface PowerManager {
+    void recharge();
+    void updateBatteryLevel(int units);
+    int getBatteryLevel();
 }
---
+++ robot/src/main/java/com/team4/robot/PowerUnit.java
@@ -0,0 +1,40 @@
+package com.team4.robot;
+
+import com.team4.commons.ConfigManager;
+
+public class PowerUnit implements PowerManager {
+
+    private int batteryLevel;
+
+    private PowerUnit() {
+        int maxBatteryLevel = Integer.parseInt(ConfigManager.getConfiguration("maxBatteryLevel"));
+        setBatteryLevel(maxBatteryLevel);
+    }
+
+    @Override
+    public void recharge() {
+        try {
+            //simulate delay while charging.
+            long timeToCharge = Long.parseLong(ConfigManager.getConfiguration("timeToCharge"));
+            Thread.sleep(timeToCharge * 1000L);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        int maxBatteryLevel = Integer.parseInt(ConfigManager.getConfiguration("maxBatteryLevel"));
+        setBatteryLevel(maxBatteryLevel);
+    }
+
+    @Override
+    public int getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    @Override
+    public void updateBatteryLevel(int units) {
+        setBatteryLevel(getBatteryLevel() - units);
+    }
+
+    private void setBatteryLevel(int batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+}
